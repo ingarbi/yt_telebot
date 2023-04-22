52b95604b8fa5f3a64f23aeced945779604904ca
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-22 10:44:53 +0300

    #2 1st command

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import telebot
 
 bot = telebot.TeleBot("5855917656:AAE89D3EgAGyP29h7c9De8E5tOLG4rfdLZM")
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.reply_to(message, "Привет %s" % message.from_user.first_name)
+
 
 bot.infinity_polling()
